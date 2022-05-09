688891ab26d54124a92608cd143bd204d3f7b445
Gustavo Valmaña Villalonga <gustavo.shotokai@gmail.com>
2022-05-09 13:20:53 -0400

    agregado un middleware para autenticar con passport

--- routes/api.php
+++ routes/api.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('oauth')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('v1/pacientes', PacienteController::class);
+Route::apiResource('v1/pacientes', PacienteController::class)->middleware('oauth');
 Route::post('v1/pacientes/validate',[PacienteController::class,'validate_model']);
 Route::post('v1/pacientes/update_multiple',[PacienteController::class,'update_multiple']);
